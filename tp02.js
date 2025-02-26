6c52bc5e44cf073e89ca354e979f6272f3c9f988
pripault <paul.ripault@gmail.com>
2025-02-26 11:56:16 +0100

    add tp02.js

---
+++ tp02.js
@@ -0,0 +1,18 @@
+var num1 = 5;
+var num2 = 3;
+console.log("addition " +  (num1 + num2))
+console.log("soustraction " +  (num1 - num2))
+console.log("multiplication " +  (num1 * num2))
+console.log("division " +  (num1 / num2))
+console.log("modulo " + +  (num1 % num2))
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 == num2)
+console.log(num1 != num2)
+var b1 = true
+var b2 = false
+console.log(b1 && b2)
+console.log(b1 || b2)
+console.log(!b1)
+console.log(!b2)
